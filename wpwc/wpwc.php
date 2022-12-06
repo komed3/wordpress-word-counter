2c326c6662ced0b8d4f6e4ddcfa26c13e2fc3c32
komed3 <pk97@outlook.de>
2022-12-06 09:33:42 +0100

    first files

---
+++ wpwc/wpwc.php
@@ -0,0 +1,15 @@
+<?php
+
+    /*
+     * Plugin Name: WordPress word counter
+     * Plugin URI: https://github.com/komed3/wordpress-word-counter
+     * Description: WordPress content word counter
+     * Author: Paul Köhler (komed3)
+     * Author URI: https://komed3.de
+     * Version: 0.01 [ALPHA] @ 12/06/2022
+     * Text Domain: wpwc
+     */
+
+    require_once __DIR__ . '/src/functions.php';
+
+?>
